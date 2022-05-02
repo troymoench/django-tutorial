454a55198219dcfa685ff117232aa6aaf83e7b2e
Troy Moench <brewersarenumber1@gmail.com>
2022-05-01 22:53:43 -0500

    Fixed Question models. was_published_recently was accidentally added to the Choice model

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -11,6 +11,9 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -19,6 +22,3 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
-
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
