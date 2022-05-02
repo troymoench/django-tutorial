a6a019f8fab958996ee2db5289cb0ff54b9b5225
Troy Moench <brewersarenumber1@gmail.com>
2022-05-01 20:00:33 -0500

    Django Tutorial Part 2: Polls App

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # django-tutorial
+
+## Database Setup
+```sql
+create user django createdb password 'password';
+```
+
+```bash
+createdb django-tutorial -h localhost -U django
+```
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "polls.apps.PollsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -75,8 +76,10 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": "localhost",
+        "NAME": "django-tutorial",
+        "USER": "django",
     }
 }
 
@@ -105,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "America/Chicago"
 
 USE_I18N = True
 
